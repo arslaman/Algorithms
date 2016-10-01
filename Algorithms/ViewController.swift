2a23862ec6c6d61ab81669c42c9c87cb0421b52d
Ruslan Mansurov <mansurov.master@gmail.com>
2016-10-01 16:19:49 +0300

    Initial project

---
+++ Algorithms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithms
+//
+//  Created by Руслан Мансуров on 01/10/16.
+//  Copyright © 2016 Ruslan Mansurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
